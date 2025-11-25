85f4a3d98aa77607574ea4214abf0a5498dd2c40
Tanbir <pondit.tanbir@gmail.com>
2025-11-25 13:29:52 +0600

    add: fix db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
             SupplierTableSeeder::class,
             ProductTableSeeder::class,
             UserTableSeeder::class,
+            DeliveryChargeSeeder::class,
+            OrderTableSeeder::class,
         ]);
     }
 }
