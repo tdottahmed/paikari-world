526e61ba668fbe01efbc8543b599f6ea3f0eb0f9
Tanbir <pondit.tanbir@gmail.com>
2025-12-18 16:16:19 +0600

    fix: iundividal discount once again

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -3,9 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Setting extends Model
 {
     protected $fillable = ['key', 'value'];
-    //
+
+    /**
+     * Clear cached setting value when this model changes.
+     */
+    protected static function booted()
+    {
+        static::saved(function ($setting) {
+            Cache::forget('setting_' . $setting->key);
+        });
+
+        static::deleted(function ($setting) {
+            Cache::forget('setting_' . $setting->key);
+        });
+
+        // Only register restored event if model uses SoftDeletes
+        if (in_array(\Illuminate\Database\Eloquent\SoftDeletes::class, class_uses_recursive(static::class))) {
+            static::restored(function ($setting) {
+                Cache::forget('setting_' . $setting->key);
+            });
+        }
+    }
 }
